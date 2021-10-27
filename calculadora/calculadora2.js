1797a61fdb75ea158c0b2cbe7d695950eaed1c25
Dorishow <jsantosalmeida54@gmail.com>
2021-10-26 22:32:49 -0300

    Potenciação refatorada

--- calculadora/calculadora2.js
+++ calculadora/calculadora2.js
@@ -1,47 +1,94 @@
 const calculadora = (() => {
-    let calculo = {n1: 0, operador: '', n2: 0}
+    let calc = {n1: false, operator: false, n2: false}
     let history = []
 
-    const resetCalc = () => calculo = {n1: 0, operador: '', n2: 0}
-    
-    const enter = data =>{
-        const {n1, operador, n2} = calculo
+    const resetCalc = () => calc = {n1: false, operator: false, n2: false}
 
-        if(typeof data == 'number'){
-            if(!n1) return calculo.n1 = data
-            if(!n2) return calculo.n2 = data
+    const isNumber = data => typeof data == 'number'
+
+    const isString = data => typeof data == 'string'
+
+    const addToHistory = (data) => history = [...history, data]
+
+    const list = () => {console.table(history)}
+
+    const reset = () => {history = []}
+    
+
+    const enter = data =>{
+        if(isNumber(data)){
+            if(!calc.n1) return calc.n1 = data
+            if(!calc.n2) return calc.n2 = data
         }
-        else if(typeof data == 'string'){
-            if(!operador) return calculo.operador = data
+        else if(isString(data)){
+            if(!calc.operator) return calc.operator = data
         } 
-        
+        else{console.log('invalid data Type')}
     } 
 
-    const addToHistory = (data) =>{
-        history = [...history, data]
+
+    function getResultOf({n1, operator, n2}){
+        const result = `${n1} ${operator} ${n2} = ${calculate(calc)}`
+        console.log(result)
+        return result
     }
 
+
     const equals = () => {
-        const {n1, operador, n2} = calculo
-        if(n1 && operador && n2){
-            const resultado = (`${n1} ${operador} ${n2} = ${calculate(calculo)}`)
-            console.log(resultado)
-            addToHistory(resultado)
+        const {n1, operator, n2} = calc
+        if(n1 && operator && n2){
+            const result = getResultOf(calc)
+            addToHistory(result)
             resetCalc()
         } else{
             console.log('Please type number 1, operator and number 2 before request the resolution')
         }
     }
 
-    const calculate = ({n1, operador, n2}) => {
-        if(operador == '+') return n1 + n2
-        if(operador == '-') return n1 - n2
-        if(operador == '*') return n1 * n2
-        if(operador == '/') return n1 / n2
+    const calculate = ({n1, operator, n2}) => {
+        if(operator == '+') return n1 + n2
+        if(operator == '-') return n1 - n2
+        if(operator == '*') return n1 * n2
+        if(operator == '/') return n1 / n2
+        if(operator === '^') return Math.pow(n1, n2)
+        else return 'operator inválido'
     }
-
-    const list = () => {console.log(history)}
-    const reset = () => {history = []}
 
     return {enter, equals, list, reset}
 })()
+
+calculadora.enter(5)
+calculadora.enter('+')
+calculadora.enter(1)
+calculadora.equals()
+
+calculadora.enter(2)
+calculadora.enter('-')
+calculadora.enter(1)
+calculadora.equals()
+
+calculadora.enter(2)
+calculadora.enter('^')
+calculadora.enter(3)
+calculadora.equals()
+
+calculadora.enter(3)
+calculadora.enter('^')
+calculadora.enter(2)
+calculadora.equals()
+
+calculadora.enter(5)
+calculadora.enter('*')
+calculadora.enter(2)
+calculadora.equals()
+
+calculadora.enter(5)
+calculadora.enter('/')
+calculadora.enter(2)
+calculadora.equals()
+
+calculadora.list()
+
+calculadora.reset()
+
+calculadora.list()
